509872acfb7345ce7f636c90f77c48838f468746
cubebert-github <cubebertoffical@gmail.com>
2024-08-18 16:45:07 -0700

    Moved the old lexicon-dm repo here
    -Moved the old lexicon-dm repo here

--- Compile.bat
+++ Compile.bat
@@ -1,138 +1,15 @@
 @echo off
 
+set FileName=Lexicon-Deathmatch
 
-:Init
-
-set FileName=LexiconDM
-set MusFileName=LexiconDM-miscdata
-
-set Compression=9
-
-set Path_BCS=Compiler
-set Path_Script=..\pk3\ACS\Lexicon
-set Path_7z=..\Compiler\7za
-set Path_Backwards=..
-
-set GotoBCS=Compile_Main
-set GotoCompileMain=Compile_Music
-
-
-if "%PROCESSOR_ARCHITECTURE%" == "AMD64" ( 
-	set Path_BCS=Compiler\x64
-	set Path_7z=..\Compiler\x64\7za
-	set Path_Backwards=..\..\
-	set Path_Script=..\..\pk3\ACS\Lexicon
-)
-
-echo CPU: "%PROCESSOR_ARCHITECTURE%"
-echo.
-echo Please choose a compile option.
-echo Note: All options will compile BCS scripts.
-echo.
-echo      Full Compression (slow):
-echo           1: Compile Everything
-echo           2: Compile Main PK3
-echo           3: Compile Music PK3
-echo.
-echo      No Compression (fast):
-echo           4: Compile Everything
-echo           5: Compile Main PK3
-echo           6: Compile Music PK3
-echo.
-echo      BCS:
-echo           7: Compile only BCS scripts.
-echo.
-Choice /c 1234567 /N /M Choose:
-
-
-
-If ERRORLEVEL 7 goto Compile_BCS_Only
-
-If ERRORLEVEL 6 goto None_Compile_Music
-If ERRORLEVEL 5 goto None_Compile_Main_Only
-If ERRORLEVEL 4 goto None_Compile_BCS
-
-If ERRORLEVEL 3 goto Compile_Music_only
-If ERRORLEVEL 2 goto Compile_Main_Only
-If ERRORLEVEL 1 goto Compile_BCS
-
-
-
-:Compile_BCS_Only
-	set GotoBCS=Finish
-	goto Compile_BCS
-
-
-
-
-
-:None_Compile_Main_Only
-	set Compression=0
-:Compile_Main_Only
-	set GotoCompileMain=Replace
-	goto Compile_BCS
-
-
-
-:None_Compile_Music
-	set Compression=0
-:Compile_Music_Only
-	set GotoBCS=Compile_Music
-	goto Compile_BCS
-
-
-
-
-
-:None_Compile_BCS
-	set Compression=0
-:Compile_BCS
-echo BCS
-	cd %Path_BCS%
-	bcc -acc-err-file -acc-stats -x bcs %Path_Script%.c %Path_Script%.o
-	goto %GotoBCS%
-
-
-
-
-:Compile_Main
-echo PACK GAME FILES
-	cd %Path_Backwards%
+echo Step 1: PACK
 	cd pk3
-	%Path_7z% a -r -ssw -mx%Compression% -tzip  ..\%FileName%n.pk3 *
+	..\compiler\7za a -r -ssw -mx9 -tzip  ..\%FileName%-New.pk3 *.kvx *.cmp *.x* *.txt *.o *.acs *.png *.wad *.md2 *.md3 *.tga *.mp3 *.pal *.bmp *.map *.lmp *.raw *.mus *.mid *.ogg *.vgz *.wav *.mod *.it *.xm *.s3m *.psm *.pk3 *.gl
+
+
+echo Step 2: REPLACE
 	cd ..
 	del %FileName%.pk3
-	ren %FileName%n.pk3 %FileName%.pk3
-	cd pk3\acs
-	goto %GotoCompileMain%
+	ren %FileName%-New.pk3 %FileName%.pk3
 
-
-
-:Compile_Music
-echo PACK MUSIC AND MISC. FILES
-	cd %Path_Backwards%
-	cd music_pk3
-	%Path_7z% a -r -ssw -mx%Compression% -tzip  ..\%MusFileName%n.pk3 *
-	cd ..
-	del %MusFileName%.pk3
-	ren %MusFileName%n.pk3 %MusFileName%.pk3
-	goto Finish
-
-
-
-
-
-
-
-
-
-:Error1
-echo Error: Could not determain CPU type, please check that your system's environment variables are accurate.
-goto Finish
-
-:Error2
-echo BCS Error: 
-goto Init
-
-:Finish
 pause
--- README.md
+++ README.md
@@ -1,21 +1,12 @@
-# LexiconDM
-LexiconDM is similar to base Lexicon, however, LexiconDM's focus is on compiling the ultimate collection of Doom Deathmatch mapsets to be played online with full support of all competitive game modes (Deathmatch/Duel/etc). The target source port is Zandronum, but LexiconDM will happily work on (G)ZDoom and other source ports based off of recent versions in the ZDoom family. LexiconDM should also work with all major gameplay mods (Samsara: Reincarnation & Insanity Deathmatch, to name two).
+# Lexicon Deathmatch Expansion Pack
+Will put info later
 
-## How do I build the PK3?
-There are two .bat files that will build the project. It is recommended that you use CompilePK3-Jenkins.bat. It will automatically build the PK3 for you and allow you to quickly test LexiconDM using your source port of choice.
+## How to build the pk3?
+Will put info later
 
-## How/Where can I play this?
-You can download it right here in our downloads section, or get it through your favorite WAD repo (also through Doomseeker/WADseeker/etc). You can also play locally with bots utilizing offline skirmish mode within Zandronum.
-
-You can also compile a build yourself from the above instructions, though builds compiled this way will be considered bleeding edge as many elements may be unfinished and/or unstable.
-
-## How can I get involved with this project?
-
-If one compiles a build per the instructions and you would be interested in filing bug reports, please feel free to do so, as this would greatly help with LexiconDM's development. You may file bug reports here, or within The Mythotic Tinker-Station Discord, along with any mapset requests.
-
-If one is interested in becoming a beta tester and/or wishes to help with development, please get in touch (include coding knowledge/skills/etc if wanting to assist development) with meleemario via The Mythotic Tinker-Station Discord or through Direct Message.
-
-Here is a link to The Mythotic Tinker-Station Discord: https://discord.gg/mwFZvHe
+## Where can I play this?
+Will put info later
 
 ## Credits
-LexiconDM is maintained by meleemario, cubebert, Shadowlink223, Shakar-a'tar, and The Proverbial Derp. For credits involving the mapsets/WADs included in LexiconDM, have a look inside the CREDITS subfolder.
+
+Insert credits here
